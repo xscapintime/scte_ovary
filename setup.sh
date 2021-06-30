2a8551c2e3ec8682a5029086fc94d6f9e43641dd
xscapintime <louiesxscape@gmail.com>
2021-06-30 22:53:45 +0800

    setup the environment

---
+++ setup.sh
@@ -0,0 +1,17 @@
+## conda environment
+conda create --name scte r=3.6
+conda activate scte
+conda isntall python=3.8
+
+## just for jupyter
+conda install -y jupyter
+
+
+conda install matplotlib
+conda install anndata2ri
+conda install -c conda-forge leidenalg
+
+
+##=== imortmant ===##
+pip install scanpy
+conda install -c conda-forge rpy2
