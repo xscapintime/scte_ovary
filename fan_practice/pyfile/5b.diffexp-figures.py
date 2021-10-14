b3ed014dd432fdf87e53920606afc045a5faa0eb
Ally <leihuajie1022@126.com>
2021-10-14 14:32:32 +0800

    fan_pyfile

---
+++ fan_practice/pyfile/5b.diffexp-figures.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import logging, matplotlib, os, sys, glob
+import scanpy as sc
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+from matplotlib import colors
+import pandas as pd
+from glbase3 import genelist
+
+
+# In[2]:
+
+
+plt.rcParams['figure.figsize']=(8,8)
+sc.settings.verbosity = 3
+sc.set_figure_params(dpi=200, dpi_save=200)
+matplotlib.rcParams['pdf.fonttype']=42
+matplotlib.rcParams['font.size']=10
+
+
+# In[3]:
+
+
+from glbase3 import genelist, glload
+
+
+# In[4]:
+
+
+sc.settings.figdir = 'diffexp'
+
+
+# In[5]:
+
+
+import os
+ 
+def mkdir(path):
+ 
+    folder = os.path.exists(path)
+ 
+    if not folder:                   #判断是否存在文件夹如果不存在则创建为文件夹
+        os.makedirs(path)            #makedirs 创建文件时如果路径不存在会创建这个路径
+        print("---  new folder...  ---")
+        print("---  OK  ---")
+ 
+    else:
+        print("---  There is this folder!  ---")
+        
+gls = "/mnt/e/projects/scte-gpc/scte_ovary/gls"
+mkdir(gls)
+
+
+# In[6]:
+
+
+[os.remove(f) for f in glob.glob('{0}/*.pdf'.format(sc.settings.figdir))]
+[os.remove(f) for f in glob.glob('gls/*.glb'.format(sc.settings.figdir))]
+[os.remove(f) for f in glob.glob('gls/*.tsv'.format(sc.settings.figdir))]
+
+
+# In[7]:
+
+
+de_leiden = 'leiden_r0.10'
+
+
+# In[8]:
+
+
+adata = sc.read('./de.h5ad')
+
+
+# In[9]:
+
+
+sc.pl.rank_genes_groups(adata, n_genes=25, sharey=True, show=False, save='genes-top25.pdf')
+sc.pl.rank_genes_groups(adata, key='rank_genes_groups', show=False, save='genes.pdf')
+sc.pl.rank_genes_groups_dotplot(adata, key='rank_genes_groups', show=False, save='genes-top25.pdf')
+
+
+# In[10]:
+
+
+print(pd.DataFrame(adata.uns['rank_genes_groups']['names']))
+
+
+# In[11]:
+
+
+topall = pd.DataFrame(adata.uns['rank_genes_groups']['names']) # get all;
+fcs = pd.DataFrame(adata.uns['rank_genes_groups']['logfoldchanges'])
+padj = pd.DataFrame(adata.uns['rank_genes_groups']['pvals_adj'])
+
+
+# In[12]:
+
+
+# Matrix of DE genes:
+
+groups = list(topall.columns.values)
+
+newcols = {}
+
+for group in groups:
+    newcols[group] = []
+
+    t = zip([i[group] for i in adata.uns['rank_genes_groups']['names']], [i[group] for i in adata.uns['rank_genes_groups']['logfoldchanges']], [i[group] for i in adata.uns['rank_genes_groups']['pvals_adj']])
+
+    print('Group: {0}'.format(group))
+    print(t)
+
+    for item in t:
+        if item[1] < 2: # fold change
+            continue
+        if item[2] > 0.01: # just in case
+            continue
+
+        newcols[group].append({'name': item[0], 'log2FC': item[1], 'q': item[2]})
+
+
+# In[13]:
+
+
+# join all and draw a dotplot:
+for group in newcols:
+    print(newcols[group])
+    if newcols[group]:
+        gl = genelist() 
+        gl.load_list(newcols[group])
+        gl.saveTSV('gls/de_genes-grp{0}.tsv'.format(group))
+        gl.save('gls/de_genes-grp{0}.glb'.format(group))
+
+        genes = [i['name'] for i in newcols[group]]
+        #sc.pl.dotplot(adata, genes, groupby=de_leiden, dot_max=0.7, dendrogram=True, standard_scale='var', show=False, save='de-grp{0}.pdf'.format(group))
+        #sc.pl.matrixplot(adata, genes, groupby=de_leiden, dendrogram=True, standard_scale='var', show=False, save='de-grp{0}.pdf'.format(group))
+
+
+# In[14]:
+
+
+for grp in newcols:
+    if not newcols[grp]:
+        continue
+    for k in newcols[grp]:
+        title = k['name']
+        sc.pl.umap(adata, color=k['name'], size=20, legend_loc='on data',
+            title=title,
+            vmin=0, vmax=3,
+            show=False, save='-markers-grp{0}-{1}.pdf'.format(grp, k['name']))
+        #sc.pl.violin(adata, [k], groupby='disease', size=0, log=False, cut=0, show=False, save='markers-{0}-disease.pdf'.format(k))
+        #sc.pl.violin(adata, [k], groupby='cell_type', size=0, log=False, cut=0, show=False, save='markers-{0}-cell_type.pdf'.format(k))
+
+
+# In[ ]:
+
+
+
+
